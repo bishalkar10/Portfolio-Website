9bf828c931c801e689d90d7a25706378d9e6f994
Bishal Kar <112653391+bishalkar10@users.noreply.github.com>
2023-08-24 01:47:45 +0000

    Fixed a typo in the stylesheet import link

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import '../src/style/index.sass'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "../src/Style/index.sass";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
